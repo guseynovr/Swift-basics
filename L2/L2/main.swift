d5b3023ed38f32a7d60a27a73c6cd3c15d495ec6
guseynov <rslnguseynov@gmail.com>
2019-05-09 20:11:28 +0300

    clean master L2

--- L2/L2/main.swift
+++ L2/L2/main.swift
@@ -8,38 +8,5 @@
 
 import Foundation
 
-var array = Array<Int>()
-
-for i in 0...541 {
-    array.append(i)
-}
-
-array[1] = 0
-
-for i in array {
-    
-    if i == 0 {
-        continue
-    }
-    
-    if let index = array.firstIndex(of: i) {
-        
-        for p in stride(from: index + i, to: array.count, by: i) {
-            
-            array[p] = 0
-        }
-    }
-}
-
-var result = Array<Int>()
-
-for i in array {
-    if i != 0 {
-        result.append(i)
-    }
-}
-
-print("result.count = " + String(result.count) + "\n")
-print(result)
 
 
