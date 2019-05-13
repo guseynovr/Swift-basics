05f79c142d7cf025dbcb83f510dca86837d4e917
guseynov <rslnguseynov@gmail.com>
2019-05-14 01:24:47 +0300

    add lesson3

---
+++ L3/L3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L3
+//
+//  Created by Ruslan Guseynov on 14/05/2019.
+//  Copyright © 2019 Ruslan Guseynov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
