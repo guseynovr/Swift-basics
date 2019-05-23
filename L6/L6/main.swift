777520d0ac40335bc174191fd9c224698bfe1981
guseynov <rslnguseynov@gmail.com>
2019-05-24 01:42:32 +0300

    completed lesson 6

--- L6/L6/main.swift
+++ L6/L6/main.swift
@@ -8,5 +8,61 @@
 
 import Foundation
 
+struct Queue<T> {
+    
+    private var elements: [T] = []
+    
+    mutating func enqueue(_ element: T) {
+        elements.append(element)
+    }
+    
+    mutating func dequeue() -> T? {
+        return elements.count > 0 ? elements.removeFirst() : nil
+    }
+    
+    func filtered(by condition: (T) -> Bool) -> Queue<T> {
+        var filteredElements: Queue<T> = Queue<T>()
+        
+        for element in elements where condition(element) {
+            filteredElements.enqueue(element)
+        }
+        
+        return filteredElements
+    }
+    
+    func peek() -> T? {
+        return elements.first
+    }
+    
+    func getSize() -> Int {
+        return elements.count
+    }
+    
+    subscript(index: Int) -> T? {
+        return elements.indices.contains(index) ? elements[index] : nil
+    }
+
+    subscript(indexes: Int ...) -> Queue<T?> {
+        var result: Queue<T?> = Queue<T?>()
+        
+        for index in indexes {
+            result.enqueue(elements.indices.contains(index) ? elements[index] : nil)
+        }
+        
+        return result
+    }
+}
+
+//MARK: Tests
+
+var q: Queue<Int> = Queue<Int>()
+for i in 0...4 {
+    q.enqueue(i)
+}
+print(q.filtered { $0 % 2 == 0 })
+print(q[2], q[10])
+print(q[0, 2, 3, 5])
+print(q.dequeue())
+print(q)
 
 
